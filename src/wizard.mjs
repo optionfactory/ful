ed248655d227ddc66c3c74e59496bba98d63b41d
Roberto Ferranti <roberto.ferranti@gmail.com>
2022-08-09 17:59:20 +0200

    :bug: progress handling for wizards; :ref: forms

--- src/wizard.mjs
+++ src/wizard.mjs
@@ -1,67 +1,70 @@
 class Wizard {
     constructor(el) {
         this.el = el;
-        this.progress = [...el.querySelectorAll('header,ol,ul')];
-        
+        this.progress = [...el.children].filter(e => e.matches("header,ol,ul"));
+
         this.progress.forEach(p => {
-            const current = p.querySelector(':scope > .active');
-            if (current === null){
-                p.children[0].classList.add('active');
+            const children = [...p.children];
+            const current = children.filter(e => e.matches(".active"))[0];
+            if (current === undefined && children.length > 0) {
+                children[0].classList.add('active');
             }
         });
         if (this.el.querySelector('section.current') === null) {
             const firstSection = this.el.querySelector('section:first-of-type');
-            if(firstSection !== null){
+            if (firstSection !== null) {
                 firstSection.classList.add('current');
             }
         }
     }
     next() {
         this.progress.forEach(p => {
-            const current = p.querySelector(':scope > .active');
-            current.classList.remove('active');
-            current.nextElementSibling.classList.add('active');
+            const children = [...p.children];
+            const current = children.filter(e => e.matches(".active"))[0];
+            current?.classList.remove('active');
+            current?.nextElementSibling?.classList.add('active');
         });
         const currentSection = this.el.querySelector('section.current');
         currentSection.classList.remove("current");
         currentSection.nextElementSibling.classList.add('current');
-        
+
         this.el.dispatchEvent(new CustomEvent('wizard:activate', {
-            bubbles: true, 
+            bubbles: true,
             cancelable: true
         }));
-        
+
     }
     prev() {
         this.progress.forEach(p => {
-            const current = p.querySelector(':scope > .active');
-            current.classList.remove('active');
-            current.previousElementSibling.classList.add('active');
-        });        
+            const children = [...p.children];
+            const current = children.filter(e => e.matches(".active"))[0];
+            current?.classList.remove('active');
+            current?.previousElementSibling?.classList.add('active');
+        });
         const currentSection = this.el.querySelector('section.current');
         currentSection.classList.remove("current");
-        currentSection.previousElementSibling.classList.add('current');        
+        currentSection.previousElementSibling.classList.add('current');
         this.el.dispatchEvent(new CustomEvent('wizard:activate', {
-            bubbles: true, 
+            bubbles: true,
             cancelable: true
         }));
     }
     moveTo = function (n) {
         this.progress.forEach(p => {
-            const current = p.querySelector(':scope > .active');
+            const children = [...p.children];
+            const current = children.filter(e => e.matches(".active"))[0];
             current?.classList.remove('active');
-            p.children[+n].classList.add('active');
-        });                
+            p.children[+n]?.classList.add('active');
+        });
         const currentSection = this.el.querySelector('section.current');
         currentSection?.classList.remove("current");
         const nthSection = this.el.querySelector(`section:nth-child(${+n})`);
         nthSection.classList.add('current');
         this.el.dispatchEvent(new CustomEvent('wizard:activate', {
-            bubbles: true, 
+            bubbles: true,
             cancelable: true
         }));
-    };
-
+    }
 }
 
 
