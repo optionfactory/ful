cbded31a3690f26e58de8208b34f5212a43393b2
Roberto Ferranti <roberto.ferranti@gmail.com>
2022-08-19 12:25:44 +0200

    :bug: wrong method in AuthorizationCodeFlowInterceptor

--- src/oauth-authorization-code.mjs
+++ src/oauth-authorization-code.mjs
@@ -28,7 +28,7 @@ class AuthorizationCodeFlow {
             state: state,
             verifier: pkceVerifier
         });
-        let url = new URL(this.authUri);
+        const url = new URL(this.authUri);
         url.searchParams.set("client_id", this.clientId);
         url.searchParams.set("redirect_uri", this.redirectUri);
         url.searchParams.set("response_type", 'code');
@@ -40,11 +40,11 @@ class AuthorizationCodeFlow {
     }
     async _tokenExchange(code, state) {
         window.history.replaceState('', "", this.redirectUri);
-        let stateAndVerifier = this.storage.pop(AuthorizationCodeFlow.PKCE_AND_STATE_KEY);
+        const stateAndVerifier = this.storage.pop(AuthorizationCodeFlow.PKCE_AND_STATE_KEY);
         if (stateAndVerifier.state !== state) {
             throw new Error("State mismatch");
         }
-        let response = await fetch(this.tokenUri, {
+        const response = await fetch(this.tokenUri, {
             method: "POST",
             headers: {
                 "Content-Type": 'application/x-www-form-urlencoded'
@@ -59,10 +59,10 @@ class AuthorizationCodeFlow {
             ])
         });
         if (!response.ok) {
-            let text = await response.text();
+            const text = await response.text();
             throw new Error("Error:" + response.status + ": " + text);
         }
-        let token = await response.json();
+        const token = await response.json();
         return new AuthorizationCodeFlowSession(this.clientId, token, this.tokenUri, this.logoutUri, this.redirectUri);
     }
 
@@ -103,7 +103,7 @@ class AuthorizationCodeFlowSession {
         this.refreshCallback = callback;
     }
     async refresh() {
-        let response = await fetch(this.tokenUri, {
+        const response = await fetch(this.tokenUri, {
             method: "POST",
             headers: {
                 "Content-Type": 'application/x-www-form-urlencoded'
@@ -139,7 +139,7 @@ class AuthorizationCodeFlowSession {
         await this.refresh();
     }
     logout() {
-        let url = new URL(this.logoutUri);
+        const url = new URL(this.logoutUri);
         url.searchParams.set("post_logout_redirect_uri", this.redirectUri);
         url.searchParams.set("id_token_hint", this.token.id_token);
         window.location = url;
@@ -164,13 +164,13 @@ class AuthorizationCodeFlowInterceptor {
         this.gracePeriodAfter = gracePeriodAfter || 30000;
     }
     async before(request) {
-        await this.session.refreshIfNeeded(this.gracePeriodBefore);
+        await this.session.refreshIf(this.gracePeriodBefore);
         const headers = new Headers(request.options.headers);
         headers.set("Authorization", this.session.bearerToken());
         return request;
     }
     async after(request, response) {
-        await this.session.refreshIfNeeded(this.gracePeriodAfter);
+        await this.session.refreshIf(this.gracePeriodAfter);
         return response;
     }
 }
