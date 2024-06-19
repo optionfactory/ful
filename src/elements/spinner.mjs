07eb9fb6f495ab9f97e77fd75371499f4713f7dd
Roberto Ferranti <roberto.ferranti@gmail.com>
2024-06-19 14:44:03 +0200

    :ref: spinner styles

--- src/elements/spinner.mjs
+++ src/elements/spinner.mjs
@@ -1,10 +1,22 @@
-import { Fragments, Templated } from "./elements.mjs"
+import { Attributes, Fragments, Templated } from "./elements.mjs"
 
-class Spinner extends Templated(HTMLElement) {
+
+
+const ful_spinner_ec = globalThis.ec || ftl.EvaluationContext.configure({
+
+});
+
+const ful_spinner_template_ = globalThis.ful_spinner_template || ftl.Template.fromHtml(`
+    <div class="ful-spinner-wrapper">
+        <div class="ful-spinner-text">{{{{ slotted.default }}}}</div>
+        <div class="ful-spinner-icon"></div>
+    </div>
+`, ful_spinner_ec);
+
+
+class Spinner extends Templated(HTMLElement, ful_spinner_template_) {
     render(slotted, template) {
-        return Fragments.fromHtml(`
-            <div class="spinner-border spinner-border-sm" aria-hidden="true"></div>
-        `);
+        return template.render({ slotted });
     }
     static configure() {
         customElements.define('ful-spinner', Spinner);
