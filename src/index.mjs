627ed90245e62371435ea200100f3cdc820fb2ce
Roberto Ferranti <roberto.ferranti@gmail.com>
2025-02-17 21:36:41 +0100

    :new: moved Deferred to ftl

--- src/index.mjs
+++ src/index.mjs
@@ -4,5 +4,4 @@ export { MediaType, HttpClient, HttpClientError } from "./http-client.mjs";
 export { LocalStorage, SessionStorage, VersionedStorage } from "./storage.mjs";
 export { AuthorizationCodeFlow, AuthorizationCodeFlowSession, AuthorizationCodeFlowInterceptor } from "./oauth-authorization-code.mjs";
 export { timing } from "./timing.mjs";
-export { Deferred } from "./deferred.mjs";
 export * from "./elements/index.mjs"
